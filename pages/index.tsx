e53377ada87fcb5089c6dcff3bb3ef38a254b35a
Daniel Itwaru <ditwaru@siterx.com>
2022-05-07 12:40:07 -0400

    adding bulleted list

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,6 +10,7 @@ const Home: NextPage = () => {
           <h2 className='text-3xl font-bold text-center'>Stage Manager Portfolio</h2>
           <StyledAnchorTags href='https://docs.google.com/document/d/11tC0RPoHwDbIvfnfaw6w6hTkwOShZK3d07B2BK1XvC0/edit' text='Rehearsal Reports' />
           <StyledAnchorTags href='https://docs.google.com/spreadsheets/d/1rcTiMSpdgpX6CJFXLomh_IoG_KYl89cdPZztSaSbDv4/edit?usp=sharing' text='Set Change Assignments' />
+          <StyledAnchorTags href='https://docs.google.com/document/d/1EmuBUPor2RxwMdhgwifHNrY643-XafGtR7T2qP77yVo/edit?usp=sharing' text='Bulleted List of Duties' />
           <StyledAnchorTags href='https://docs.google.com/document/d/1RxvZHDwPSwhfk9glbruYg6C_88qLhtA0MfTjw9wJzxM/edit' text='Rehearsal Calendar' />
           <StyledAnchorTags href='https://docs.google.com/document/d/1GYaOXB7zR85dSikoJVNraiGm8BQroNca2aIFPSdJlJM/edit' text='Props List' />
           <StyledAnchorTags href='https://docs.google.com/forms/d/1l2j3CNAyDqfiwKUS-oI2D_5rwg6bYa62bCs59DH4q4E/edit' text='Cast Superlatives' />
